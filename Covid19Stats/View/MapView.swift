145557514bdcb5b8e1f24e7e60f3549943589728
Burak Tunç <buraktuncdev@gmail.com>
2020-07-07 18:15:55 +0300

    MapView is OK

--- Covid19Stats/View/MapView.swift
+++ Covid19Stats/View/MapView.swift
@@ -6,10 +6,54 @@
 //  Copyright © 2020 Burak Tunç. All rights reserved.
 //
 
-import Foundation
+import SwiftUI
 import UIKit
 import MapKit
 
 struct MapView: UIViewRepresentable {
+    
+   
+    
+  
+    @Binding var countryData: [CountryData]
+    
+    func updateUIView(_ uiView: MKMapView, context: UIViewRepresentable<MapView>) {
+        
+        
+        var allAnnotation: [CoronaCaseAnnotation] = []
+        
+        for data in countryData {
+            
+            let title = data.country + "\n Confirmed " + data.confirmed.formatNumber() + "\n Death " + data.deaths.formatNumber()
+            
+            let coordinate = CLLocationCoordinate2D(latitude: data.latitude, longitude: data.longitude)
+            
+            allAnnotation.append(CoronaCaseAnnotation(title: title, coordinate: coordinate))
+            
+            uiView.annotations.forEach( {uiView.removeAnnotation($0) } )
+            uiView.addAnnotation(allAnnotation as! MKAnnotation)
+            
+        }
+        
+        func makeUIView(context:UIViewRepresentable<MapView>) -> MKMapView {
+            return MKMapView()
+        }
+        
+    }
+    
+    
+}
+
+
+class CoronaCaseAnnotation: NSObject, MKAnnotation {
+    
+    let coordinate: CLLocationCoordinate2D
+    let title: String?
+    
+    init(title: String?, coordinate: CLLocationCoordinate2D) {
+        self.title = title
+        self.coordinate = coordinate
+    }
+    
     
 }
